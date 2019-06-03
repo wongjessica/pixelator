20e1da9350dca3c3f9339d67b7fe506ef8cd0216
Jessica Wong <wong.jessica0411@gmail.com>
2019-06-03 02:57:38 -0400

    test

--- text.js
+++ text.js
@@ -14,3 +14,101 @@ function myCreateFunction() {
   function mouseOver() {
 	alert("Hey, I told you not to hover over me!");
 }
+
+let rowCount = 0;
+let colCount = 16;
+let cellsTable = document.getElementById("cells");
+
+function rowPlus() {
+  let newRow = document.createElement("tr");
+  for (let i = 0; i < colCount; i++) {
+    let newCell = document.createElement("td");
+    newCell.addEventListener("click",colorCell);
+    newCell.addEventListener("mouseover",colorCellDrag);
+    newRow.appendChild(newCell);
+  }
+  cellsTable.appendChild(newRow);
+  rowCount++;
+};
+
+function rowMinus() {
+  cellsTable.removeChild(cellsTable.lastChild);
+  rowCount--;
+};
+
+function colPlus() {
+  let children = Array.from(cellsTable.children);
+  children.forEach((row) => {
+    let newCell = document.createElement("td");
+    newCell.addEventListener("click",colorCell);
+    newCell.addEventListener("mouseover",colorCellDrag);
+    row.appendChild(newCell);
+  });
+  colCount++;
+};
+
+function colMinus() {
+  let children = Array.from(cellsTable.children);
+  children.forEach((row) => {
+    row.removeChild(row.lastChild);
+  });
+  colCount--;
+};
+
+let colorSelector = document.getElementById("select-color");
+let color;
+function updateColor() {
+  color = colorSelector.value;
+};
+updateColor();
+
+function fillAll(){
+  let tableChildren = Array.from(cellsTable.children);
+  tableChildren.forEach((row) => {
+    let rowChildren = Array.from(row.children);
+    rowChildren.forEach((cell)=>{
+      cell.style.backgroundColor =color;
+
+    });
+  });
+};
+
+function fillEmpty(){
+  let tableChildren = Array.from(cellsTable.children);
+  tableChildren.forEach((row) => {
+    let rowChildren = Array.from(row.children);
+    rowChildren.forEach((cell)=>{
+
+      if(cell.style.backgroundColor == ""){
+
+        cell.style.backgroundColor =color;
+      }
+      //console.log(cell.style.backgroundColor);
+
+    });
+  });
+};
+
+function clearCells(){
+  let tableChildren = Array.from(cellsTable.children);
+  tableChildren.forEach((row) => {
+    let rowChildren = Array.from(row.children);
+    rowChildren.forEach((cell)=>{
+      cell.style.backgroundColor ="";
+
+    });
+  });
+};
+
+function colorCell(){
+  console.log(this);
+  this.style.backgroundColor = color;
+};
+
+function colorCellDrag(param){
+  if(param.buttons){
+
+   this.style.backgroundColor = color;
+  }
+
+};
